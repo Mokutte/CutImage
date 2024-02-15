778680c9f7441f7008770d109600b80ce6e86615
Mokutekigaru <feromantrain@gmail.com>
2024-02-15 17:42:49 +0400

    first commit

---
+++ ios/ImageCropperModule.m
@@ -0,0 +1,8 @@
+//
+//  ImageCropperModule.m
+//  
+//
+//  Created by Георгий Беридзе on 15.02.24.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/ImageCropperModule.swift
@@ -0,0 +1,8 @@
+//
+//  ImageCropperModule.swift
+//  
+//
+//  Created by Георгий Беридзе on 15.02.24.
+//
+
+import Foundation
